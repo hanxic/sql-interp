9fac023a461e67fcd26d39f13d4f9767b8556969
Hanxi Chen <garychen2000@outlook.com>
2023-11-19 03:04:27 -0500

    src/SQLPrinter.hs
    Checkpoint: pretty on Bool, String, Int, and Uop

--- src/SQLPrinter.hs
+++ src/SQLPrinter.hs
@@ -20,7 +20,21 @@ pretty = PP.render . pp
 oneLine :: (PP a) => a -> String
 oneLine = PP.renderStyle (PP.style {PP.mode = PP.OneLineMode}) . pp
 
+instance PP Bool where
+  pp True = PP.text "TRUE"
+  pp False = PP.text "False"
+
+instance PP String where
+  pp = PP.text
+
+instance PP Int where
+  pp = PP.int
+
 instance PP Uop where
+  pp Not = PP.text "NOT"
+  pp Neg = PP.char '-'
+
+instance PP Bop where
   pp = undefined
 
 instance PP DValue where
